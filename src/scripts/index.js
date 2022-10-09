459acc47b1ce1c7eaea1b6dd84580b8e82f417e5
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-10-09 21:07:13 +0500

    add reactor link, model info

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,16 @@ let totalModelsCount
 let loadedModelsCount = 0
 let loadingStatus
 let loaderBlur
+let modelInfo
+let modelNameContainer
 
 document.addEventListener("DOMContentLoaded", () => {
     totalModelsCount = document.querySelectorAll("a-marker").length
     loadingStatus = document.querySelector(".loading-model-status")
     loaderBlur = document.querySelector(".preloader-blur")
+    modelInfo = document.querySelector(".model-info-bar")
+    modelNameContainer = modelInfo.querySelector("p")
+
     loadingStatus.textContent = `Загружено моделей: 0/${totalModelsCount}`
 })
 
@@ -26,3 +31,17 @@ AFRAME.registerComponent('foo', {
         })
     }
 })
+
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+        this.el.addEventListener("markerFound", () => {
+            modelNameContainer.textContent = this.el.id
+            modelInfo.classList.add("active")
+        })
+        this.el.addEventListener("markerLost", () => {
+            modelInfo.classList.remove("active")
+            modelNameContainer.textContent = ""
+        })
+    }
+})
+
